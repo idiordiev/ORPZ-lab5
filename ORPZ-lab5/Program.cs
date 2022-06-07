ef1795757cd21e6a6be466b8b510e50186026831
Ivan Diordiev <i.diordiev@outlook.com>
2022-06-07 15:29:25 +0300

    Initial commit

---
+++ ORPZ-lab5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ORPZ_lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
